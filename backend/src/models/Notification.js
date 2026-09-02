ab94a401f78bee2ebe23f20fe3ec10f434e7412a
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-09-02 12:35:14 +0100

    creating the notification model

---
+++ backend/src/models/Notification.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        "SYSTEM",
+        "ORDER",
+        "REFERRAL",
+        "COMMISSION",
+        "WITHDRAWAL",
+      ],
+      default: "SYSTEM",
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification = mongoose.model(
+  "Notification",
+  notificationSchema
+);
+
+export default Notification;
