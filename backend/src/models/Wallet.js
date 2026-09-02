7bd616f064c559d1782205272c94ce23fddd7232
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-09-02 12:32:29 +0100

    creating the wallet model

---
+++ backend/src/models/Wallet.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const walletSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+
+    availableBalance: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    pendingBalance: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    totalEarned: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Wallet = mongoose.model("Wallet", walletSchema);
+
+export default Wallet;
