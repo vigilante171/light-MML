4710f8f388c71d65132087679c4942a253f1cd97
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-09-02 12:21:39 +0100

    implemetent the data base connection via mongo db

---
+++ backend/src/server.js
@@ -0,0 +1,53 @@
+import connectDatabase from "./config/database.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+dotenv.config()
+const app= express()
+const PORT = process.env.PORT||5000
+app.use(cors({
+    origin:process.env.CLIENT_URI||"http://localhost5173",
+    credentials:true,
+})
+)
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+app.use("/api", limiter);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Product MLM API is running",
+    environment: process.env.NODE_ENV,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+const startServer = async () => {
+  await connectDatabase();
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
